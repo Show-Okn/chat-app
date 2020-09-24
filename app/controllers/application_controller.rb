a2b6ecd4be3978040e2f2878674968a2ee1981ec
Show-Okn <okataro2626@gmail.com>
2020-09-24 19:20:16 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_primitted_parameters, if: :devise_controller?
+
+  private
+  def configure_primitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
