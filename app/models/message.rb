1a6026b4a812da1496059439661c3d64c08c66dc
Show-Okn <okataro2626@gmail.com>
2020-09-28 20:00:52 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,13 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+<<<<<<< Updated upstream
+=======
+  has_one_attached :image
+  validates :content, presence: true, unless: :was_attached?
+>>>>>>> Stashed changes
 
-  validates :content, presence: true
+  def was_attached?
+    self.image.attached?
+  end
 end
